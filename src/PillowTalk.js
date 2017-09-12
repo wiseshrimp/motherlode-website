2383bf79bcf2348eb1c7d1d973a466ab54ac5421
Sue Roh <sr3022@columbia.edu>
2017-09-12 18:47:17 -0400

    Made styling changes

--- src/PillowTalk.js
+++ src/PillowTalk.js
@@ -5,16 +5,21 @@ export default class PillowTalk extends React.Component {
   render() {
     return (
       <div className="pillow-talk animate-in">
+        <img alt="pillowtalk" id="pillow-talk-img" src={require('./assets/images/pillowtalk.png')} />
         <div className="pillow-text">
-          Conventional sex education underrepresents the spectrum of sexual identity, and what is lacking often leads to misinformation from alternative sources.
-          <br />
+          <div className="s-text">
+            Conventional sex education underrepresents the spectrum of sexual identity, and what is lacking often leads to misinformation from alternative sources.
+          </div>
           Pillow Talk is a computer-generated virtual reality experience that covers sex positive topics which are often omitted from traditional sex education curriculum. These topics include, but are not limited to, inclusivity within the vast sexual spectrum, in and out of the bedroom, and counteracting stigmas.
-          <br />
-          Pillow Talk will act as an interactive platform that welcomes and celebrates the vast field of sexuality by including marginalized perspectives that have systematically been excluded from the conversation. This experience will be developed for the HTC Vive and Oculus Rift.
-
-          We acknowledge the fact that we do not have the right to speak on behalf of other people’s sexual identities and experiences. Therefore, we will implement our artistic and technical skills to act act as developers rather than educators.
-          <br />
-          Pillow Talk is a collaborate project, welcoming any and all ideas encompassing sexuality. We are seeking input, critique, and suggestions as Pillow Talk develops and materializes. Please feel free to contact Motherlode - your voice is indispensible.
+          <div className="s-text">
+            Pillow Talk will act as an interactive platform that welcomes and celebrates the vast field of sexuality by including marginalized perspectives that have systematically been excluded from the conversation. This experience will be developed for the HTC Vive and Oculus Rift.
+          </div>
+          <div className="s-text">
+            We acknowledge the fact that we do not have the right to speak on behalf of other people’s sexual identities and experiences. Therefore, we will implement our artistic and technical skills to act act as developers rather than educators.
+          </div>
+          <div className="s-text">
+            Pillow Talk is a collaborate project, welcoming any and all ideas encompassing sexuality. We are seeking input, critique, and suggestions as Pillow Talk develops and materializes. Please feel free to contact Motherlode - your voice is indispensible.
+          </div>
         </div>
       </div>
     )
