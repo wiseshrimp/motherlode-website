c1150d60c89df3ca33595bbe785b077303a78755
Sue Roh <sr3022@columbia.edu>
2017-09-14 17:38:30 -0400

    Hash historry

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Router, Route, Switch } from 'react-router-dom'
+import { Route, HashRouter } from 'react-router-dom'
 
 import About from './AboutUs'
 import Contact from './Contact'
@@ -9,18 +9,16 @@ import Navbar from './Navbar'
 import PillowTalk from './PillowTalk'
 
 const Routes = (
-  <Router>
+  <HashRouter>
     <div>
       <Navbar />
-      <Switch>
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
         <Route path="/pillow-talk" component={PillowTalk} />
         <Route path="/login" component={Login} />
         <Route exact path="/" component={Home} />
-      </Switch>  
     </div>
-  </Router>
+  </HashRouter>
 )
 
 export default Routes
