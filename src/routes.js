efd2b8e8fa56a31d0db90f637adaf2cb9acfe309
Sue Roh <sr3022@columbia.edu>
2017-09-14 17:09:07 -0400

    Updated routes

--- src/routes.js
+++ src/routes.js
@@ -12,11 +12,11 @@ const Routes = (
   <BrowserRouter>
     <div>
       <Navbar />
-      <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/pillow-talk" component={PillowTalk} />
-      <Route path="/login" component={Login} />
-      <Route exact path="/" component={Home} />
+      <Route path={`${process.env.PUBLIC_URL}/about`} component={About} />
+      <Route path={`${process.env.PUBLIC_URL}/contact`} component={Contact} />
+      <Route path={`${process.env.PUBLIC_URL}/pillow-talk`} component={PillowTalk} />
+      <Route path={`${process.env.PUBLIC_URL}/login`} component={Login} />
+      <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home} />
     </div>
   </BrowserRouter>
 )
