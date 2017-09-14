1ca405b584553235760a46628a594bb0afb4ea45
Sue Roh <sr3022@columbia.edu>
2017-09-14 17:33:07 -0400

    Defined Switch

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Router, Route } from 'react-router-dom'
+import { Router, Route, Switch } from 'react-router-dom'
 
 import About from './AboutUs'
 import Contact from './Contact'
